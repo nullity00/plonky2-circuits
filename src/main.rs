f174353b12cc964649090b14e17534e7c31cc58e
nullity00 <reshmashaik3011@gmail.com>
2023-09-06 20:53:15 +0530

    Co-authored-by: Sachin <sachin@setprotocol.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,52 @@
-use plonky2::plonk::{circuit_data::CircuitConfig, circuit_builder::CircuitBuilder};
+use anyhow::Result;
+use num::integer::Roots;
+use plonky2::field::types::Field;
+use plonky2::iop::witness::{ PartialWitness, WitnessWrite };
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::CircuitConfig;
+use plonky2::plonk::config::{ GenericConfig, PoseidonGoldilocksConfig };
+use plonky2::plonk::circuit_data::CircuitData;
 use plonky2::field::goldilocks_field::GoldilocksField;
-use plonky2::plonk::config::PoseidonGoldilocksConfig;
-use plonky2::iop::witness::{PartialWitness, Witness}; 
 
-type F = GoldilocksField;
+const D: usize = 2;
 type C = PoseidonGoldilocksConfig;
+type F = <C as GenericConfig<D>>::F;
 
-fn main() {
-    // Set up 
+fn some_function_in_zk(x_inp: u64) -> (CircuitData<GoldiLocksField, PoseidonGoldilocksConfig, 2>, PartialWitness<GoldiLocksField>) {
     let config = CircuitConfig::standard_recursion_config();
-    let mut builder = CircuitBuilder::<F, 2>::new(config.clone());
+    let mut builder = CircuitBuilder::<F, D>::new(config);
 
-    // build circuit, a - b = d 
-    // a = 3, b = 3, d = 0
-    let user_t = builder.add_virtual_target();
-    let verifier_t = builder.add_virtual_target();
+    // Defining the circuit (wires and gates).
+    let square_root = builder.add_virtual_target();
+    let x_target = builder.add_virtual_target();
 
-    let lhs_t = builder.sub(user_t, verifier_t);
+    let mut pw = PartialWitness::new();
+    let x = F::from_canonical_u64(x_inp);
+    pw.set_target(x_target, x);
+    pw.set_target(square_root, F::from_canonical_u64(x_inp.sqrt()));
 
-    let d_t = builder.add_virtual_target();
-    builder.connect(lhs_t, d_t);
-    let data = builder.build::<C>();
+    // Constraint
+    let z = builder.mul(square_root, square_root);
+    let isz = builder.sub(x_target, z);
+    builder.assert_zero(isz);
 
-    // assign witness data
-    let mut pw = PartialWitness::<F>::new();
-    pw.set_target(user_t, GoldilocksField(3));
-    pw.set_target(verifier_t, GoldilocksField(3));
-    pw.set_target(d_t, GoldilocksField(0));
+    builder.register_public_input(square_root);
+    builder.register_public_input(x_target);
 
-    //proof 
-    let proof = data.prove(pw).unwrap();
-    match data.verify(proof) {
-        Ok(()) => println!("Proof Success!"),
-        Err(x) => println!("{}", x)
-    }
+    let data: CircuitData<
+        GoldilocksField,
+        PoseidonGoldilocksConfig,
+        2
+    > = builder.build::<C>();
+    (data, pw)
+}
+
+fn main() -> Result<()> {
+    // Tests
+    let (prover, pw) = some_function_in_zk(16);
+    let proof = prover.prove(pw)?;
+
+    println!("Factorial starting at {} is {}", proof.public_inputs[0], proof.public_inputs[1]);
+
+    prover.verify(proof)
 }
